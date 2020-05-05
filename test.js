f1409cceb770003c07788b13f9693ccaf59a056c
Greg Dolley <gregdolley@gmail.com>
2020-05-04 17:46:38 -0700

    Operator tests

--- test.js
+++ test.js
@@ -2,6 +2,12 @@ const process = require('process');
 
 function main() {
     console.log("Platform = ", process.platform);
+
+    console.log("if(\"\") returns:", "" ? true : false);
+    console.log("Double NOT (!!) on blank string returns:", !!"");
+    console.log("Double NOT (!!) on empty object returns:", !!{});
+    console.log("Double NOT (!!) on null returns:", !!null);
+    console.log("Double NOT (!!) on undefined returns:", !!undefined);
 }
 
 main();
