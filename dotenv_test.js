b9a05167d54d62d1f42d634181976b240d38198d
gregdolley <gregdolley@gmail.com>
2022-12-15 03:29:35 -0800

    The right way to check for DEBUG environment

--- dotenv_test.js
+++ dotenv_test.js
@@ -3,16 +3,25 @@ require("dotenv").config();
 console.log("process.env.DEBUG =", process.env.DEBUG);
 console.log("process.env.NAME =", process.env.NAME);
 
-let debug =
-  typeof process.env.DEBUG === "string"
-    ? process.env.DEBUG.trim().toLowerCase() === "true" ||
-      process.env.DEBUG.trim() === "1"
-      ? true
-      : false
-    : Boolean(process.env.DEBUG);
+let debug = isDebugEnv();
 
-if (debug === true) console.debug("hello", process.env.NAME);
+if (debug === true) console.debug("DEBUG environment set.");
+else console.log("DEBUG environment not set.");
 
-// require("dotenv").config({ debug: true });
+console.log("process.env.NODE_ENV =", process.env.NODE_ENV);
 
-// console.log("hello", process.env.NAME);
+// Returns true if the DEBUG environment variable is set to either string values:
+//
+// "true" (not case-sensitive, "TRUE" is equivalent)
+// "1"
+//
+// OR, the process.env.DEBUG property is set to one of the following:
+//
+// true (the primitive type, not the string "true")
+// 1
+// or any value the Boolean() function would eval as "truthy".
+//
+// Returns false in all other cases.
+function isDebugEnv() {
+  return typeof process.env.DEBUG === 'string' ? (process.env.DEBUG.trim().toLowerCase() === 'true' || process.env.DEBUG.trim() === '1' ? true : false) : Boolean(process.env.DEBUG);
+}
